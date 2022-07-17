4248a015704e6344c20698856d781555d6e03b3c
hkinnkun <86373472+hkinnkun@users.noreply.github.com>
2022-07-17 21:13:38 +0800

    update

--- 217_Contains_Duplicate.java
+++ 217_Contains_Duplicate.java
@@ -3,3 +3,15 @@ class Solution {
         return nums.length != Arrays.stream(nums).boxed().distinct().collect(Collectors.toList()).size();
     }
 }
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-1; i++) {
+            if(nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
